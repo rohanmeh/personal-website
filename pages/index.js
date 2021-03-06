8e9ed50be5af86e7ec18e5e30fb54fe661f73033
Rohan Mehta <rohan.mehta@accenture.com>
2021-03-06 14:01:35 -0500

    Update home page with personal links

--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,49 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Rohan Mehta</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Rohan Mehta
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Serverless Architect
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://dev.to/rohanmehta_dev" className={styles.card}>
+            <h3>Blog &rarr;</h3>
+            <p>Posts on application architecture</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://github.com/rohanmeh" className={styles.card}>
+            <h3>GitHub &rarr;</h3>
+            <p>Side projects and prototypes</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://www.linkedin.com/in/rohan-mehta-1052b570/"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>LinkedIn &rarr;</h3>
+            <p>Connect with me on LinkedIn</p>
+          </a>
+
+          <a
+            href="https://twitter.com/rohanmehta_dev"
+            className={styles.card}
+          >
+            <h3>Twitter &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Tweets on application architecture
             </p>
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
